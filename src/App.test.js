71ed9d42060c844ea2ae5a45d63f7c6c154cf001
김택주 <wpaltm12@naver.com>
2022-05-31 18:03:29 +0900

    +, - 버튼 테스트 코드 추가

--- src/App.test.js
+++ src/App.test.js
@@ -8,3 +8,17 @@ test('the counter starts at 0', () => {
 
   expect(counterElement).toHaveTextContent(0);
 });
+
+test("minus button has correct text", () => {
+	render(<App/>);
+	const minusButtonElement = screen.getByTestId("minus-button");
+
+	expect(minusButtonElement).toHaveTextContent("-");
+})
+
+test("plus button has correct text", () => {
+	render(<App/>);
+	const plusButtonElement = screen.getByTestId("plus-button");
+
+	expect(plusButtonElement).toHaveTextContent("+");
+})
